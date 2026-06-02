37096e5547910db37d1c4411e4ed229f1546eb4e
husseinnohm-maker <hussein.nohm@gmail.com>
2026-06-02 16:11:26 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,13 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
 @layer base {
   :root {
